3eef00e6dce92fc868e2060b6588555c134957ec
Сабина <sabinaisakova165@gmail.com>
2024-04-09 15:31:12 +0300

    4 апреля

---
+++ SABINA/HOMEwork/15473138/8.py
@@ -0,0 +1,14 @@
+for i in range(100, 1000):
+    s = str(i)
+    a1 = int(s[0])
+    a2 = int(s[1])
+    a3 = int(s[2])
+    pr1 = a1 + a2
+    pr2 = a2 + a3
+    if pr1<pr2:
+        n = str(pr1) + str(pr2)
+    else:
+        n = str(pr2) + str(pr1)
+    if n == '812':
+        print(i)
+        break
