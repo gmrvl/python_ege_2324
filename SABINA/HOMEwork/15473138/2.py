1778b6ef5a48381c567710529e06d7b9eacd634e
Сабина <sabinaisakova165@gmail.com>
2024-04-09 15:19:26 +0300

    4 апреля

--- SABINA/HOMEwork/15473138/2.py
+++ SABINA/HOMEwork/15473138/2.py
@@ -1,7 +1,13 @@
-count = 0
-for i in range(1000, 10000):
+for i in range(100, 1000):
     s = str(i)
     a1 = int(s[0])
     a2 = int(s[1])
     a3 = int(s[2])
-    a4 = int(s[3])
+    pr1 = a1 * a2
+    pr2 = a2 * a3
+    if pr1<pr2:
+        n = str(pr1) + str(pr2)
+    else:
+        n = str(pr2) + str(pr1)
+    if n == '621':
+        print(i)
