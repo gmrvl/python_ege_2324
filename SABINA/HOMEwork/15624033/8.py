e8741eaa1f24d6f2e713a1a34e6b8e5360e1451c
agumerova <gmrvl@mail.ru>
2024-04-21 12:07:10 +0300

    Сабина 21 апреля

--- SABINA/HOMEwork/15624033/8.py
+++ SABINA/HOMEwork/15624033/8.py
@@ -5,6 +5,7 @@ for i in range(1000,10000):
     a2 = s[1]
     a3 = s[2]
     a4 = s[3]
+    # дописать условие
     sum1 = int(a1) + int(a2)
     sum2 = int(a3) + int(a4)
     if sum1 < sum2:
@@ -13,4 +14,4 @@ for i in range(1000,10000):
         n = str(sum2) + str(sum1)
     if n == '616':
         count += 1
-        print(count)
+print(count)
