184115ee7005c446a43fb6e29dd72368c92c1d6e
agumerova <gmrvl@mail.ru>
2024-05-10 16:57:14 +0300

    Сабина 10 мая

---
+++ SABINA/6/47210.py
@@ -0,0 +1,14 @@
+from turtle import *
+k = 30
+left(90)
+speed(100)
+for i in range(7):
+    forward(10*k)
+    right(120)
+pu()
+for x in range(0, 15):
+    for y in range(0, 15):
+        goto(x*k, y*k)
+        dot(5)
+done()
+
