8352d4107a65cc3eb50cfbdcf39f26c6b6a3be5e
Сабина <sabinaisakova165@gmail.com>
2024-07-08 11:17:19 +0300

    21.04

---
+++ SABINA/HOMEwork/16246872/4.py
@@ -0,0 +1,7 @@
+for i in range(60,150):
+    s = '1'*i
+    while '111' in s:
+        s = s.replace('111','2', 1)
+        s = s.replace('222','11', 1)
+    if s == '2211':
+        print(i)
---
+++ SABINA/HOMEwork/16246872/6.py
@@ -0,0 +1,16 @@
+for a in range(1,70):
+    for b in range(1,70):
+        for c in range(1,70):
+            s = '0'+ '1'*a + '2'*b +'3'*c + '0'
+            while not '00' in s:
+                s = s.replace('01','210',1)
+                s = s.replace('02','3101',1)
+                s = s.replace('03','2012',1)
+            odin = s.count('1')
+            dva = s.count('2')
+            tri = s.count('3')
+            if odin == 70 and dva == 56 and tri == 23:
+                print(a)
+                print(b)
+                print(c)
+
