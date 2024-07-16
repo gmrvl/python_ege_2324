e483916c31f0e4afbd323e2310f234df322552fe
Сабина <sabinaisakova165@gmail.com>
2024-07-16 13:44:49 +0300

    21.04

--- SABINA/HOMEwork/16246842/3.py
+++ SABINA/HOMEwork/16246842/3.py
@@ -1,6 +1,11 @@
-for i in range(1,10000):
+for i in range(1, 10000):
     s = bin(i)[2:]
-    while '0' in s or '1' in s:
-        if '0' in s:
-            s = s.replace('0','1',1)
-        if ''
+    s = s.replace('0', '*')
+    s = s.replace('1', '0')
+    s = s.replace('*', '1')
+    while s[0] == 0:
+        s = s[1:]
+    r = int(s, 2)
+    r = int(s) - int(r)
+    if s == '999':
+        print(r)
