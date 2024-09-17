98eb34126a509d8c2bd237a99c521dd4a2e297d4
agumerova <gmrvl@mail.ru>
2024-09-17 06:44:51 +0300

    Сабина 9 sep

--- SABINA/HOMEwork/демоверсия 2025/8.py
+++ SABINA/HOMEwork/демоверсия 2025/8.py
@@ -2,8 +2,13 @@ count = 0
 for i in range(12**4,12**5):
     a = ''
     while i > 0:
-        a = str(i % 12) + a
+        ost = i % 12
+        if ost > 8:
+            a = '*' + a
+        else:
+            a = str(ost) + a
         i = i // 12
-    n = '91011'
-    if a.count('7') == 1 and a.count(''):
+    if a.count('7') == 1 and a.count('*') <= 3:
+        count += 1
+print(count)
 
