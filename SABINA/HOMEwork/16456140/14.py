078f77477a40b6c660f725950bc30068d9aec2ba
agumerova <gmrvl@mail.ru>
2024-10-07 19:19:07 +0300

    Сабина 7 окт

--- SABINA/HOMEwork/16456140/14.py
+++ SABINA/HOMEwork/16456140/14.py
@@ -1,8 +1,7 @@
-a = 4**34 + 5*4**22 + 4**13 + 2*4**9 + 82
+a = 4**34 + 5*(4**22) + 4**13 + 2*(4**9) + 82
 r = ''
 ahex = hex(a)[2:]
 #while a > 0:
  #   r = str(a % 16) + r
   #  a = a // 16
-if len(set(ahex)) == len(ahex):
-    print( len(ahex))
+print(len(set(ahex)))
