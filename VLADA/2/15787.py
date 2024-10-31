59a54a85c0dce160a38f8f784e4cb71a0c56bf32
mvladlenad <mihailovavlada2007@gmail.com>
2024-10-31 19:54:05 +0300

    fffdfe

--- VLADA/2/15787.py
+++ VLADA/2/15787.py
@@ -5,6 +5,6 @@ for x in range(0, 2):
         for z in range(0, 2):
             for w in range(0, 2):
                 if (((x <= y) and (y <= w)) or (z == (x or y))) == 0:
-                    print(x, y, z, w)
+                    print (x, y, z, w)
 
 
