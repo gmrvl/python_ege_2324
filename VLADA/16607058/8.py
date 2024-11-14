bcc70bf24cdd2b352a9a919b1ebddc2292520bc8
mvladlenad <mihailovavlada2007@gmail.com>
2024-11-14 10:39:10 +0300

    домашка

--- VLADA/16607058/8.py
+++ VLADA/16607058/8.py
@@ -1,1 +1,11 @@
-решить потом !!!!
+for n in range(0, 10000):
+    bin_n = bin(n)[2:]
+    summ = bin_n.count ('1')
+    if summ % 2 == 1:
+        bin_n += '11'
+    else:
+        bin_n += '00'
+    r = int(bin_n, 2)
+    if r >= 114:
+        print(r)
+        break
