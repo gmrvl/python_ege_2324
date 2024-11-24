17ac52b5faa421c875ebc5267a999e9bae94bf7d
Sabina <sabinaisakova165@gmail.com>
2024-11-24 18:07:02 +0300

    дз

--- SABINA/HOMEwork/16919799 тип 16/7.py
+++ SABINA/HOMEwork/16919799 тип 16/7.py
@@ -4,7 +4,7 @@ def F(n):
     elif n == 2:
         return 2
     elif n > 2 and n % 2 == 0:
-        return (4*n - F(n-3)) / 8
+        return int((4*n - F(n-3)) / 8)
     elif n > 2 and n % 2 == 1:
-        return (4*n - F(n-1) + F(n-3)) / 8
+        return int((4*n - F(n-1) + F(n-2)) / 8)
 print(F(52)-F(38))
---
+++ SABINA/HOMEwork/16919799 тип 16/8.py
@@ -0,0 +1,6 @@
+def F(n):
+    if n == 1:
+        return 1
+    elif n > 1:
+        return F(n-1) + n
+print(F(40))
