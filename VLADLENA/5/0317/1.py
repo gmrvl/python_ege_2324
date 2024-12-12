eb67f165e09e9a258748f7d3877c04c9b6098fa1
mvladlenad <mihailovavlada2007@gmail.com>
2024-12-12 16:12:43 +0300

    домашка

---
+++ VLADLENA/5/0317/1.py
@@ -0,0 +1,7 @@
+for n i range ( 1000, 100000):
+    bin_n = bin (n) [2:]
+    summ % 2 == 0
+        bin_n = '1' + '2'
+        bin_n = '3' + '4'
+        sums = sorted [bin_n_1, bin_n_2]
+        bin_n = 'bin_n_1' + 'bin_n_2'
