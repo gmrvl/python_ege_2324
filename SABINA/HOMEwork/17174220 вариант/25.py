8b742be61afef3466900d89cdfee2cfe3803074e
agumerova <gmrvl@mail.ru>
2025-01-14 17:57:41 +0300

    Сабина 13 янв

--- SABINA/HOMEwork/17174220 вариант/25.py
+++ SABINA/HOMEwork/17174220 вариант/25.py
@@ -1,10 +1,12 @@
 # 12??1*56
 for i in range(10):
-    d = int('12'+ str(i)+str(i)+'156')
-    if d <= (10**8) and d % 317 == 0:
-        print(d, d // 317)
-for i in range(10):
-    for j in range(0,1000):
-        d = int('12'+ str(i)+str(i)+'1'+ str(j)+ '56')
-        if d <= (10**8) and d % 317 == 0:
+    for j in range(10):
+        d = int('12'+ str(i)+str(j)+'156')
+        if d % 317 == 0:
             print(d, d // 317)
+for i in range(10):
+    for j in range(0,10):
+        for k in range(0, 10):
+            d = int('12'+ str(i)+str(j)+'1'+ str(k)+ '56')
+            if d <= (10**8) and d % 317 == 0:
+                print(d, d // 317)
--- SABINA/HOMEwork/17174220 вариант/9.py
+++ SABINA/HOMEwork/17174220 вариант/9.py
@@ -1,14 +1,19 @@
 f = open('09 (9).csv')
 cnt = 0
-r = []
+r = [[],[],[],[],[],[]]
 for i in f:
     s = list(map(int, i.split(';')))
-    for k in range(0, len(s)):
-        if s.count(s[k]) == 1:
-            r.append(k)
-for l in r:
-    if s.count(r[l]) < 170:
-        cnt+=1
+    for j in range(6):
+        r[j].append(s[j])
+f = open('09 (9).csv')
+for i in f:
+    s = list(map(int, i.split(';')))
+    interes = 0
+    for j in range(6):
+        if s.count(s[j]) == 1 and r[j].count(s[j]) < 170:
+            interes += 1
+    if interes >= 4:
+        cnt += 1
 print(cnt)
 
 
