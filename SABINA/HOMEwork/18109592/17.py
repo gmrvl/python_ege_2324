54d2c91fe861ba3d1af958bac11e05995874ba8c
agumerova <gmrvl@mail.ru>
2025-04-29 18:03:32 +0300

    Сабина 27 янв

--- SABINA/HOMEwork/18109592/17.py
+++ SABINA/HOMEwork/18109592/17.py
@@ -2,8 +2,8 @@ f  = [int(x) for x in open('17.txt')]
 maxx = -1000
 r = []
 for i in range(0,len(f)-1):
-    for j in range(i,len(f)):
+    for j in range(i + 1,len(f)):
         x,y = f[i],f[j]
-        if (x-y) % 126 == 0:
+        if (x+y) % 126 == 0:
             r.append(x+y)
 print(len(r),max(r))
