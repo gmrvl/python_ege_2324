6d3b6f4b40757cf40a4cc60ecbc417ecccdf70a0
Sabina <sabinaisakova165@gmail.com>
2025-05-07 16:59:49 +0300

    дз

--- SABINA/HOMEwork/18234727/25.py
+++ SABINA/HOMEwork/18234727/25.py
@@ -1,13 +1,12 @@
 for n in range(2000000,3000001):
-    sqr = int(n**0.5)
+    sqr = int(n ** 0.5)
     r = []
-    for delit in range(1, sqr+1):
+    for delit in range(1, sqr + 1):
         if n % delit == 0:
-            if delit <= (n // delit) and (n // delit)-delit <= 115:
+            if delit <= (n // delit) and (n // delit) - delit <= 115:
                 r.append((n // delit)-delit)
             else:
-                if (n // delit)-delit <= 115:
-                    r.append(delit-(n // delit))
+                if (n // delit) - delit <= 115:
+                    r.append(delit - (n // delit))
         if len(r) >= 3:
             print(n)
-            break
